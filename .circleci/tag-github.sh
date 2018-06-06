f52bb2e86eaa5018095c95aa75e2f3f931d0a56a
Brenda Wallace <shiny@cpan.org>
2018-06-06 14:11:02 +1200

    Script for tagging github

---
+++ .circleci/tag-github.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# git tag "build-$CIRCLE_BUILD_NUM"
+# git push origin "build-$CIRCLE_BUILD_NUM"
+
+# git tag "release-$created_gh_deployment"
+# git push origin "release-$created_gh_deployment"
