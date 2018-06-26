ecdab4af6d116741deb591d8a30ae09c5420719a
Brenda Wallace <brenda@wallace.net.nz>
2018-06-26 14:15:20 +1200

    Auto corrected by following curly (#66)

--- src/components/Templates/Search.js
+++ src/components/Templates/Search.js
@@ -43,12 +43,12 @@ class Search extends Component {
       var context = this, args = arguments;
       var later = function() {
         timeout = null;
-        if (!immediate) func.apply(context, args);
+        if (!immediate) {func.apply(context, args);}
       };
       var callNow = immediate && !timeout;
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+      if (callNow) {func.apply(context, args);}
     };
   }
 
